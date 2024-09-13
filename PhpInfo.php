a8d8d3d5237a16a0718cf6479a9cb175a7b552da
GUY-WATCHO-PC\guyba <guybami@yahoo.fr>
2024-09-13 12:31:30 +0200

    - Add config init parameters - Add FTP  parameters to config file - Upload stable version to Server

--- PhpInfo.php
+++ PhpInfo.php
@@ -1,9 +1,11 @@
 <?php
 
   
-  echo "mySQL verion " . mysql_get_client_info() . "<br/>";
+  echo "mySQL verion " . mysqli_get_client_info() . "<br/>";
   echo "php Verion " . phpversion() . "<br/>";
   echo  "php PHP_BINDIR " . PHP_LIBDIR. "<br/>";
   echo  "php dirname(dirname(PHP_BINARY)) " . dirname(dirname(PHP_BINARY)) . "<br/>";
   phpinfo();
-?>
+
+
+ 
